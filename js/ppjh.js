9581455cafbda212ba74b069dfa471ea82593d15
Tyler <tkicks32@gmail.com>
2017-05-25 18:36:16 -0400

    quickpush gh-pages

---
+++ js/ppjh.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function () {
+	// matrix effect in progress
+	var myCanvas = document.getElementById("header");
+	var matrix_text = document.getElementById("matrix_name").innerHTML;
+	var ctx1 = myCanvas.getContext("2d");
+	var matrix = function() {
+		ctx1.fillStyle='rgba(0,0,0,.07)';
+		ctx1.fillRect(0,0,500,500);
+		ctx1.fillStyle="#0f0";
+		ctx1.fillText(matrix_text, Math.random()*(500), Math.random()*(500));
+	};
+	setInterval(matrix, 30);
+})*/
